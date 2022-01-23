624268b6d0eb134d4a741ac114c31d87429b79a5
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-23 15:50:32 +0530

    Update songs.py

--- CGS/plug/songs.py
+++ CGS/plug/songs.py
@@ -1,73 +1,130 @@
+
+import html
+import asyncio
+import math
 import os
-import requests
+import time
+from random import randint
+from urllib.parse import urlparse
+from Python_ARQ import ARQ
+from CGS.db.tginfo import ARQ_API_KEY, UPDATES_CHANNEL, BOT_USERNAME
+from CGS import CGS
+import aiofiles
 import aiohttp
+import requests
+import wget
 import youtube_dl
-
-from CGSBot import CGS
-from CGSBot.db.db import db
-from CGSBot.db.humanbytes import humanbytes
-from pyrogram import filters, Client
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait, MessageNotModified
+from pyrogram.types import Message
 from youtube_search import YoutubeSearch
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputTextMessageContent
 
-def time_to_seconds(time):
-stringt = str(time)
-return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
+is_downloading = False
+
+aiohttpsession = aiohttp.ClientSession()
+
+arq = ARQ("https://thearq.tech", ARQ_API_KEY, aiohttpsession)
 
 @CGS.on_message(filters.command('song'))
 def song(client, message):
 
-user_id = message.from_user.id
-user_name = message.from_user.first_name
-rpk = "["+user_name+"](tg://user?id="+str(user_id)+")"
+    user_id = message.from_user.id 
+    user_name = message.from_user.first_name 
+    rpk = "["+user_name+"](tg://user?id="+str(user_id)+")"
 
-query = ''
-for i in message.command[1:]:
-query += ' ' + str(i)
-print(query)
-m = message.reply('Searching Song 🍀🍀')
-ydl_opts = {
-  "format": "bestaudio[ext=m4a]"
-}
-try:
-results = YoutubeSearch(query, max_results = 1).to_dict()
-link = f"https://youtube.com{results[0]['url_suffix']}"
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    m = message.reply('**Now I am Searching Your Song 🔎\n\nPlease Wait 😊**')
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
         #print(results)
-title = results[0]["title"][:40]
-thumbnail = results[0]["thumbnails"][0]
-thumb_name = f'thumb {title}.jpg'
-thumb = requests.get(thumbnail, allow_redirects = True)
-open(thumb_name, 'wb').write(thumb.content)
+        title = results[0]["title"][:40]       
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f'thumb{title}.jpg'
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, 'wb').write(thumb.content)
 
-duration = results[0]["duration"]
-url_suffix = results[0]["url_suffix"]
-views = results[0]["views"]
 
-except Exception as e:
-m.edit(
-  "❌ Found Nothing.\n\nTry another keywork or maybe spell it properly."
-)
-print(str(e))
-return
-m.edit("wait download song searching now 😊😊")
-try:
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-info_dict = ydl.extract_info(link, download = False)
-audio_file = ydl.prepare_filename(info_dict)
-ydl.process_info(info_dict)
-rep = f"Song name : [ {title}/nlink :( {link})\nDurations : ` {duration}`\n\n📩 Downnload By : @CGSUPDATES '
-secmul, dur, dur_arr = 1, 0, duration.split(':')
-for i in range(len(dur_arr)-1, -1, -1):
-dur += (int(dur_arr[i]) * secmul)
-secmul *= 60
-s = message.reply_audio(audio_file, caption = rep, thumb = thumb_name, parse_mode = 'md', title = title, duration = dur)
-m.delete()
-except Exception as e:
-m.edit('❌ Error repot this error to @CGSUPDATES')
-print(e)
+        duration = results[0]["duration"]
+        url_suffix = results[0]["url_suffix"]
+        views = results[0]["views"]
 
-try:
-os.remove(audio_file)
-os.remove(thumb_name)
-except Exception as e:
-print(e)
+    except Exception as e:
+        m.edit(
+            "Nothing Found {} ☹️\n\nPlease check your spellings and try again😊".format(message.from_user.mention)
+        )
+        print(str(e))
+        return
+    m.edit("**Now I am Downloading Your Song ⏳\n\nPlease Wait 😊**")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = "🎙 <b>Title</b>: [{title[:35]}]({link})\n🎵 <b>Source</b> : <code>Youtube</code>\n⏱️ <b>Song Duration</b>: <code>{duration}</code>\n👁‍🗨 <b>Song Views</b>: <code>{views}</code>\n\n<b>Downloaded By</b> : @CGSSONGBOT\n\n<b>Requested By</b> : {} 😊".format(message.from_user.mention)
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='html', title=title, duration=dur)
+        m.delete()
+    except Exception as e:
+        m.edit(e)
+
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
+
+
+# Funtion To Download Song
+async def download_song(url):
+    song_name = f"{randint(6969, 6999)}.mp3"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(song_name, mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+    return song_name
+
+@app.on_message(filters.command("deezers") & ~filters.edited)
+async def deezsong(_, message):
+    global is_downloading
+    if len(message.command) < 2:
+        await message.reply_text("{},\n\nUse this format to download songs from deezer 👇\n\n<code>/deezer song_name</code>".format(message.from_user.mention))
+        return
+    if is_downloading:
+        await message.reply_text(
+            "{},\n\nAnother download is in progress now ⏳\n\nPlease try again after 1 or 2 minutes 😊".format(message.from_user.mention)
+        )
+        return
+    is_downloading = True
+    text = message.text.split(None, 1)[1]
+    query = text.replace(" ", "%20")
+    m = await message.reply_text("**Now I am Searching Your Song 🔎\n\nPlease Wait 😊**")
+    try:
+        songs = await arq.deezer(query, 1)
+        if not songs.ok:
+            await message.reply_text(songs.result)
+            return
+        title = songs.result[0].title
+        url = songs.result[0].url
+        artist = songs.result[0].artist
+        cap = "🎵 <b>Source</b> : <code>Deezer</code>\n\n<b>Downloaded By</b> : @CGSSONGBOT\n\nBOT BY : @CGSUPDATES/n/n<b>Requested By</b> : {} 😊".format(message.from_user.mention)
+        await m.edit("Now I am Downloading Your Song ⚡\n\nPlease Wait🌹")
+        song = await download_song(url)
+        await m.edit("Now I am Uploading Your Song ⚡\n\nPlease Wait🌹")
+        await message.reply_audio(audio=song, caption=cap, title=title, performer=artist)
+        os.remove(song)
+        await m.delete()
+    except Exception as e:
+        is_downloading = False
+        await m.edit(str(e))
+        return
+    is_downloading = False
