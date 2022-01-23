a7bcc112497ea79be80b1449bf6aa711c26d5ce9
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-23 11:19:49 +0530

    Create CGS.py

---
+++ CGS/CGS.py
@@ -0,0 +1,44 @@
+import os
+import os
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from CGS import CGS
+from CGS.__main__ import BUTTON, BUTTONSA
+
+
+   START_TEXT = """
+**HELLO I'M CGS SONG DOWNLOAD BOT**
+
+You can download song me a very fast ⚡
+
+Commands view to send /help or help button.
+
+"""
+   HELP_TEXT = """
+*Command list By CGSSONGBOT*
+
+/song - send you want download song name
+/saavn - send you want download saavn name
+/lyric - send you want see lyric name
+/video - send you want download video name
+
+Thanks users 🍀
+"""
+    
+    
+# Call backs @CGSUPDATES 
+
+@CGS.on_callback_query()
+async def cb_data(client, message):
+    if message.data == "help":
+        await message.message.edit_text(
+            text=Translation.HELP_TEXT.format(message.from_user.mention),
+            reply_markup=BUTTONSA,
+            disable_web_page_preview=True,
+        )
+    elif message.data == "helpback":
+        await message.message.edit_text(
+            text=Translation.START_TEXT.format(message.from_user.mention),
+            reply_markup=Translation.BUTTONS,
+            disable_web_page_preview=True
+        )
