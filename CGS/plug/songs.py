dd73c9da0987cfaf35a374b581c5d61b876bcd4b
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-23 13:12:05 +0530

    Update songs.py

--- CGS/plug/songs.py
+++ CGS/plug/songs.py
@@ -56,7 +56,7 @@ with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 info_dict = ydl.extract_info(link, download = False)
 audio_file = ydl.prepare_filename(info_dict)
 ydl.process_info(info_dict)
-rep = f"Song name : [ {title[:35]}]( {link})\nDurations : ` {duration}`\n\n📩 Downnload By : @CGSUPDATES '
+rep = f"Song name : [ {title}/nlink :( {link})\nDurations : ` {duration}`\n\n📩 Downnload By : @CGSUPDATES '
 secmul, dur, dur_arr = 1, 0, duration.split(':')
 for i in range(len(dur_arr)-1, -1, -1):
 dur += (int(dur_arr[i]) * secmul)
