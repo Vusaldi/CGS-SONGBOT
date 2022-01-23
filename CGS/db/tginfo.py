b0884d9ce09cd69a30a3812b446da0f5fccb6d8f
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-23 11:44:37 +0530

    Update tginfo.py

--- CGS/db/tginfo.py
+++ CGS/db/tginfo.py
@@ -1,5 +1,7 @@
 import os
-API_ID = int(os.getenv("API_ID"))
+API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ARQ_API_KEY = os.getenv("ARQ_API_KEY")
+BOT_USERNAME = os.getenv("BOT_USERNAME")
+MONGODB_URI = os.getenv("MONGODB_URI")
