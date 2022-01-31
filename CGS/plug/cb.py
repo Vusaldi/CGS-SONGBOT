802c70159a8b66292ddd4f61d50f6dd2b557458e
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-31 18:54:28 +0530

    Create cb.py

---
+++ CGS/plug/cb.py
@@ -0,0 +1,24 @@
+import os
+from CGS.exe import exe
+import config
+from pyrogram import Client
+from pyrogram.errors import UserNotParticipant
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from CGS import CGS as app
+
+# Call backs @CGSUPDATES 
+
+@app.on_callback_query()
+async def cb_data(client, message):
+    if message.data == "help":
+        await message.message.edit_text(
+            text=exe.HELP_TEXT.format(message.from_user.mention),
+            reply_markup=exe.btn,
+            disable_web_page_preview=True,
+        )
+    elif message.data == "helpback":
+        await message.message.edit_text(
+            text=exe.START_TEXT.format(message.from_user.mention),
+            reply_markup=exe.btns,
+            disable_web_page_preview=True
+        )
