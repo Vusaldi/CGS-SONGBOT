a22d85d86da4885e487c277fabfa635e9fc8aa93
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-01-31 16:24:36 +0530

    Update exe.py

--- CGS/exe.py
+++ CGS/exe.py
@@ -3,41 +3,11 @@ import os
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from CGS import CGS as app
+from CGS.__main__ import START_TEXT, HELP_TEXT, btn, btns
 
-class CGS(object):
 
-   START_TEXT = """
-HELLO I'M CGS SONG DOWNLOAD BOT
 
-You can download song me a very fast ⚡
-
-Commands view to send /help or help button.
-
-"""
-   HELP_TEXT = """
-*Command list By CGSSONGBOT*
-
-/song - send you want download song name
-/saavn - send you want download saavn name
-/lyric - send you want see lyric name
-/video - send you want download video name
-
-Thanks users 🍀
-"""
-   BUTTONS = InlineKeyboardMarkup(
-       [[
-        InlineKeyboardButton("Help 🆘", callback_data="help")
-       ],[
-        InlineKeyboardButton("UPDATES 📢", url="https://t.me/CGSUPDATES"),
-        InlineKeyboardButton("SUPPORT 💬", url="https://t.me/CGSsupport")
-       ]]
-   )
-    
-   BUTTONSA = InlineKeyboardMarkup(
-       [[     
-       InlineKeyboardButton("BACK 🍀", callback_data="help_back")
-       ]]
-   )    
+   
     
     
 # Call backs @CGSUPDATES 
